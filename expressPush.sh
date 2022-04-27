337d71fab9af0079934d89d8f52467340d752082
felipeschirmann@gmail.com <felipeschirmann@gmail.com>
2022-04-27 09:44:20 -0300

    Adjust of Title page

---
+++ expressPush.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##### ref: https://dev.to/forcetrekker/single-shell-script-command-to-stage-all-files-commit-and-push-to-current-branch-3d0h
+
+# get current branch and push
+current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+# git pull
+git pull origin "$current_branch"
+echo "====pull changes from '$current_branch' branch"
+
+# get the argument message
+message="$1"
+
+# If no commit message is passed, use current date time in the commit message
+if [[ -z "${message// }" ]]
+    then
+        message=$(date '+%Y-%m-%d %H:%M:%S')
+fi
+
+# stage all changes
+git add .
+echo "====staged all git files"
+
+# add commit
+git commit --no-verify -m "$message"
+echo "====added the commit with message: '$message'"
+
+# git push
+git push origin "$current_branch"
+echo "====pushed changes to '$current_branch' branch"
